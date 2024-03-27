cb8147d3eb343bc1d73eef1b2c06c09f942d585c
Tom <tom@contextfree.info>
2024-03-27 05:31:11 -0700

    Start water

--- godot/Player.gd
+++ godot/Player.gd
@@ -6,6 +6,7 @@ extends AnimatedSprite2D
 
 
 func _process(delta: float):
+	var animation_speed = 1.0
 	var velocity := Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
 		velocity = Vector2.UP.rotated(rotation) * 400 * delta
@@ -14,9 +15,10 @@ func _process(delta: float):
 	elif Input.is_action_pressed("ui_left"):
 		velocity = Vector2.LEFT.rotated(rotation) * 400 * delta
 	elif Input.is_action_pressed("ui_right"):
+		animation_speed = 2.0
 		velocity = Vector2.RIGHT.rotated(rotation) * 400 * delta
 	if velocity.length() > 0:
-		play("swim")
+		play("swim", animation_speed)
 		position += velocity
 	else:
 		stop()
