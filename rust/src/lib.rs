2fc8067b337127cc7faca62e3d292f4c827e0366
Tom <tom@contextfree.info>
2024-03-27 21:13:09 -0700

    Work toward rust

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,5 +1,3 @@
-use godot::engine::ISprite2D;
-use godot::engine::Sprite2D;
 use godot::prelude::*;
 
 struct MyExtension;
@@ -8,33 +6,52 @@ struct MyExtension;
 unsafe impl ExtensionLibrary for MyExtension {}
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=Node)]
 struct RustShip {
     speed: f64,
-    angular_speed: f64,
-
-    base: Base<Sprite2D>,
+    base: Base<Node>,
+    sprite: Option<Gd<Node2D>>,
+    state: State,
 }
 
 #[godot_api]
-impl ISprite2D for RustShip {
-    fn init(base: Base<Sprite2D>) -> Self {
+impl INode2D for RustShip {
+    fn init(base: Base<Node>) -> Self {
         godot_print!("Hello, world!"); // Prints to the Godot console
-
         Self {
             speed: 400.0,
-            angular_speed: std::f64::consts::PI,
             base,
+            sprite: None,
+            state: State::Wait,
         }
     }
 
     fn physics_process(&mut self, delta: f64) {
+        let sprite = self.sprite.as_mut().unwrap();
+        sprite.get_position();
         // In GDScript, this would be:
         // rotation += angular_speed * delta
 
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
+        // let radians = (self.angular_speed * delta) as f32;
+        // self.base_mut().rotate(radians);
         // The 'rotate' method requires a f32,
         // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
     }
+
+    fn ready(&mut self) {
+        self.sprite = Some(
+            self.base()
+                .get_node("Sprite".into())
+                .unwrap()
+                .cast::<Node2D>(),
+        );
+    }
+}
+
+enum State {
+    Wait,
+    Start,
+    Enter,
+    Loop,
+    Exit,
 }
