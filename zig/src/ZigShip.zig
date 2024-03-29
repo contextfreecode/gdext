37912b531af16c30fbebb3a0c4f49415190dff71
Tom <tom@contextfree.info>
2024-03-29 06:00:58 -0700

    Zag zig

--- zig/src/ZigShip.zig
+++ zig/src/ZigShip.zig
@@ -1,11 +1,56 @@
 const Godot = @import("Godot");
+const Vector2 = Godot.Vector2;
 
 const Self = @This();
 
-pub usingnamespace Godot.Sprite2D;
-godot_object: *Godot.Sprite2D,
+pub usingnamespace Godot.Node;
+godot_object: *Godot.Node,
+
+speed: f32 = 500,
+
+sprite: *Godot.Node2D = undefined,
+state: State = .wait,
+start: Vector2 = @splat(0),
+target: *Godot.Node2D = undefined,
 
 pub fn _physics_process(self: *Self, delta: f64) void {
     if (Godot.Engine.getSingleton().is_editor_hint()) return;
-    self.rotate(delta);
+    var position = self.sprite.get_position();
+    if (self.state == .wait) self.start = position;
+    const target = self.target.get_position();
+    self.state = switch (self.state) {
+        .wait => .enter,
+        .enter => if (position[0] < target[0]) .zag else self.state,
+        .zag => if (position[0] < target[0] / 2) .exit else self.state,
+        .exit => if (position[0] < -200) .wait else self.state,
+    };
+    const distance: Vector2 = @splat(@as(f32, @floatCast(delta)) * self.speed);
+    const offset = Godot.normalized(target - self.start);
+    position += distance * switch (self.state) {
+        .wait => Vector2{ 0, 0 },
+        .enter => offset,
+        .zag => Vector2{ offset[0], -offset[1] },
+        .exit => offset,
+    };
+    if (self.state == .wait) position = self.start;
+    self.sprite.set_position(position);
 }
+
+pub fn _ready(self: *Self) void {
+    if (Godot.Engine.getSingleton().is_editor_hint()) return;
+    // Sprite
+    var sprite_path = Godot.NodePath.initFromString("Sprite");
+    defer sprite_path.deinit();
+    self.sprite = @ptrCast(self.get_node(sprite_path));
+    // Target
+    var target_path = Godot.NodePath.initFromString("Target");
+    defer target_path.deinit();
+    self.target = @ptrCast(self.get_node(target_path));
+}
+
+const State = enum {
+    wait,
+    enter,
+    zag,
+    exit,
+};
