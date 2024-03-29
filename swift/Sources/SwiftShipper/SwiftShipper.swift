998ee046ed2180692e054e8e6ffd1f7b65113a87
Tom <tom@contextfree.info>
2024-03-28 17:26:12 -0700

    Move swift

--- swift/Sources/SwiftShipper/SwiftShipper.swift
+++ swift/Sources/SwiftShipper/SwiftShipper.swift
@@ -3,10 +3,45 @@ import SwiftGodot
 #initSwiftExtension(cdecl: "swift_entry_point", types: [SwiftShip.self])
 
 @Godot
-class SwiftShip: Sprite2D {
-    public override func _ready () {}
+class SwiftShip: Node {
+    public override func _ready () {
+        sprite = (getNode(path: "Sprite") as! Node2D)
+        target = (getNode(path: "Target") as! Node2D)
+    }
 
     public override func _process(delta: Double) {
-        rotation += delta
+        var position = sprite!.position
+        if (state == .wait) {
+            start = position
+        }
+        let targetPos = target!.position
+        state = switch state {
+            case .wait: .enter
+            case .enter: position.y > targetPos.y ? .exit : state
+            case .exit: position.y < start.y ? .wait : state
+        }
+        let direction = (targetPos - start).normalized()
+        let move = switch state {
+            case .wait: Vector2.zero
+            case .enter: direction
+            case .exit: Vector2(x: direction.x, y: -direction.y)
+        }
+        position += move * delta * speed
+        if (state == .wait) {
+            position = start
+        }
+        sprite!.position = position
     }
+
+    var speed = 400.0
+    var sprite = nil as Node2D?
+    var start = Vector2()
+    var state = State.wait
+    var target = nil as Node2D?
+}
+
+enum State {
+    case wait
+    case enter
+    case exit
 }
