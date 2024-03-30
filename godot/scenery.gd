efb99d54515e45458d6c9d2aa54e9fa84cd44edc
Tom <tom@contextfree.info>
2024-03-30 05:13:40 -0700

    Reset x

--- godot/scenery.gd
+++ godot/scenery.gd
@@ -26,6 +26,8 @@ func _process(delta):
 		place_tile()
 	# TODO Extend scenery. Clean up old.
 	#tile_map.get_cell_tile_data()
+	if position.x < -reset_x:
+		reset_things()
 
 
 func place_tile():
@@ -37,8 +39,26 @@ func place_tile():
 	tile_map.set_cell(0, last_tile_coords, 0, next_tile)
 
 
+func reset_things():
+	var from := start_tile()
+	position.x += reset_x
+	var to := start_tile()
+	var tile_width := tile_map.tile_set.tile_size.x
+	var needed := ceili(viewport_size.x / float(tile_width))
+	for i in range(0, needed):
+		tile_map.set_cell(
+			0, Vector2i(to.x + i, to.y),
+			0, tile_map.get_cell_atlas_coords(0, Vector2i(from.x + i, to.y)),
+		)
+
+
+func start_tile() -> Vector2i:
+	return tile_map.local_to_map(Vector2(-position.x, viewport_size.y))
+
+
 var info: Info
 var last_tile_coords: Vector2i
+var reset_x := 2000.0
 @onready var tile_map: TileMap = $TileMap
 var viewport_size: Vector2i
 
