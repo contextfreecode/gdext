5c07caac31f043fd2dea4ccb7e0b75f454b99c84
Tom <tom@contextfree.info>
2024-03-30 04:57:05 -0700

    Generate ground

--- godot/main.gd
+++ godot/main.gd
@@ -2,9 +2,13 @@ extends Node
 
 
 func _ready():
-	var sprite_info := read_sprites()
-	var tile_info := read_tiles()
-	get_tree().call_group("scenery", "prep_info", sprite_info, tile_info)
+	var scenery_info = Scenery.Info.new()
+	var rng := RandomNumberGenerator.new()
+	rng.seed = 1337
+	scenery_info.rng = rng
+	scenery_info.sprite_info = read_sprites()
+	scenery_info.tile_info = read_tiles()
+	get_tree().call_group("scenery", "prep_info", scenery_info)
 
 
 func read_sprites() -> Scenery.SpriteInfo:
@@ -19,7 +23,8 @@ func read_sprites() -> Scenery.SpriteInfo:
 				info.min_scale = min(info.min_scale, scale)
 				info.max_scale = max(info.max_scale, scale)
 				print(kid.name, ": ", scale, ", ", path)
-				info.refs[path] = kid
+				if path not in info.refs:
+					info.refs[path] = kid.duplicate()
 	print(info.refs)
 	print(info.min_scale, ", ", info.max_scale)
 	return info
@@ -27,27 +32,28 @@ func read_sprites() -> Scenery.SpriteInfo:
 
 func read_tiles() -> Scenery.TileInfo:
 	var info := Scenery.TileInfo.new()
-	var tiles := $Ground/TileMap as TileMap
-	var tile_set := tiles.tile_set
+	var tile_map := $Ground/TileMap as TileMap
+	var tile_set := tile_map.tile_set
 	var source := tile_set.get_source(0) as TileSetAtlasSource
 	for i in range(0, source.get_tiles_count()):
 		var tile_id := source.get_tile_id(i)
 		#var alt := source.get_alternative_tile_id(i)
 		var region := source.get_tile_texture_region(tile_id, 0)
 		print(tile_id, ": ", region)
-	var coords_array := tiles.get_used_cells(0)
+	var coords_array := tile_map.get_used_cells(0)
 	coords_array.sort()
-	var last := Vector2.ZERO
+	var last := Vector2i.ZERO
 	for coords in coords_array:
-		var data := tiles.get_cell_tile_data(0, coords)
-		var atlas_coords := tiles.get_cell_atlas_coords(0, coords)
-		var current_nexts := info.nexts.get(last, []) as Array
-		if last != Vector2.ZERO and atlas_coords not in current_nexts:
+		var data := tile_map.get_cell_tile_data(0, coords)
+		var atlas_coords := tile_map.get_cell_atlas_coords(0, coords)
+		var current_nexts := \
+			info.nexts.get(last, [] as Array[Vector2i]) as Array[Vector2i]
+		if last != Vector2i.ZERO and atlas_coords not in current_nexts:
 			current_nexts.append(atlas_coords)
 			info.nexts[last] = current_nexts
 		#data.set_custom_data()
-		print(coords, ": ", atlas_coords, ", ", last, ", ", current_nexts)
-		#tiles.set_cell(0, coords)
+		#print(coords, ": ", atlas_coords, ", ", last, ", ", current_nexts)
+		#tile_map.set_cell(0, coords)
 		last = atlas_coords
 	print(info.nexts)
 	return info
--- godot/scenery.gd
+++ godot/scenery.gd
@@ -2,29 +2,51 @@ class_name Scenery
 extends Node2D
 
 
-@export_range(100, 1000, 50) var speed := 500.0
+@export_range(100, 1000, 50) var speed := 400.0
 
 
-func prep_info(sprite_info_: SpriteInfo, tile_info_: TileInfo):
-	sprite_info = sprite_info_
-	tile_info = tile_info_
-	print("prep_info: ", name, ", ", sprite_info, ", ", tile_info)
+func prep_info(info_: Info):
+	info = info_
+	print("prep_info: ", name, ", ", info_)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var coords_array := tile_map.get_used_cells(0)
+	last_tile_coords = coords_array.max()
+	viewport_size = get_window().size
+	print(last_tile_coords, ": ", tile_map.map_to_local(last_tile_coords))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x -= delta * speed
+	var viewport_right := viewport_size.x + position.x
+	if last_tile_coords.x < viewport_right + 500:
+		place_tile()
+	# TODO Extend scenery. Clean up old.
 	#tile_map.get_cell_tile_data()
 
 
-var sprite_info: SpriteInfo
-var tile_info: TileInfo
-@onready var tile_map = $TileMap
+func place_tile():
+	var current := tile_map.get_cell_atlas_coords(0, last_tile_coords)
+	var nexts: Array[Vector2i] = info.tile_info.nexts[current]
+	var next_index := info.rng.randi_range(0, nexts.size() - 1)
+	var next_tile = nexts[next_index]
+	last_tile_coords = Vector2i(last_tile_coords.x + 1, last_tile_coords.y)
+	tile_map.set_cell(0, last_tile_coords, 0, next_tile)
+
+
+var info: Info
+var last_tile_coords: Vector2i
+@onready var tile_map: TileMap = $TileMap
+var viewport_size: Vector2i
+
+
+class Info:
+	var rng: RandomNumberGenerator
+	var sprite_info: SpriteInfo
+	var tile_info: TileInfo
 
 
 class SpriteInfo:
@@ -34,4 +56,4 @@ class SpriteInfo:
 
 
 class TileInfo:
-	var nexts: Dictionary # Vector2, Array#[Vector2]
+	var nexts: Dictionary # Vector2i, Array[Vector2i]
