856699995ddd8d2cac3fcd722519a9be76787edd
Tom <tom@contextfree.info>
2024-03-29 20:58:06 -0700

    Deliver info to scenery

--- godot/main.gd
+++ godot/main.gd
@@ -2,30 +2,31 @@ extends Node
 
 
 func _ready():
-	read_sprites()
-	read_tiles()
-	get_tree().call_group("scenery", "prep_info")
+	var sprite_info := read_sprites()
+	var tile_info := read_tiles()
+	get_tree().call_group("scenery", "prep_info", sprite_info, tile_info)
 
 
-func read_sprites():
+func read_sprites() -> Scenery.SpriteInfo:
+	var info := Scenery.SpriteInfo.new()
 	var nodes := get_tree().get_nodes_in_group("scenery")
 	var refs := {} # String, Sprite
-	var min_scale := INF
-	var max_scale := 0.0
 	for node in nodes:
 		for kid in node.get_children():
 			if kid is Sprite2D:
 				var path := kid.texture.resource_path as String
 				var scale := kid.scale.x as float
-				min_scale = min(min_scale, scale)
-				max_scale = max(max_scale, scale)
+				info.min_scale = min(info.min_scale, scale)
+				info.max_scale = max(info.max_scale, scale)
 				print(kid.name, ": ", scale, ", ", path)
-				refs[path] = kid
-	print(refs)
-	print(min_scale, ", ", max_scale)
+				info.refs[path] = kid
+	print(info.refs)
+	print(info.min_scale, ", ", info.max_scale)
+	return info
 
 
-func read_tiles():
+func read_tiles() -> Scenery.TileInfo:
+	var info := Scenery.TileInfo.new()
 	var tiles := $Ground/TileMap as TileMap
 	var tile_set := tiles.tile_set
 	var source := tile_set.get_source(0) as TileSetAtlasSource
@@ -36,20 +37,20 @@ func read_tiles():
 		print(tile_id, ": ", region)
 	var coords_array := tiles.get_used_cells(0)
 	coords_array.sort()
-	var nexts := {} # Vector2, Array[Vector2]
 	var last := Vector2.ZERO
 	for coords in coords_array:
 		var data := tiles.get_cell_tile_data(0, coords)
 		var atlas_coords := tiles.get_cell_atlas_coords(0, coords)
-		var current_nexts := nexts.get(last, []) as Array
+		var current_nexts := info.nexts.get(last, []) as Array
 		if last != Vector2.ZERO and atlas_coords not in current_nexts:
 			current_nexts.append(atlas_coords)
-			nexts[last] = current_nexts
+			info.nexts[last] = current_nexts
 		#data.set_custom_data()
 		print(coords, ": ", atlas_coords, ", ", last, ", ", current_nexts)
 		#tiles.set_cell(0, coords)
 		last = atlas_coords
-	print(nexts)
+	print(info.nexts)
+	return info
 
 
 func _on_cpp_ship_position_changed(node: Node, new_pos: Vector2):
--- godot/scenery.gd
+++ godot/scenery.gd
@@ -1,11 +1,14 @@
+class_name Scenery
 extends Node2D
 
 
 @export_range(100, 1000, 50) var speed := 500.0
 
 
-func prep_info():
-	print("prep_info: ", name)
+func prep_info(sprite_info_: SpriteInfo, tile_info_: TileInfo):
+	sprite_info = sprite_info_
+	tile_info = tile_info_
+	print("prep_info: ", name, ", ", sprite_info, ", ", tile_info)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +19,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.x -= delta * speed
+	#tile_map.get_cell_tile_data()
 
 
+var sprite_info: SpriteInfo
+var tile_info: TileInfo
 @onready var tile_map = $TileMap
+
+
+class SpriteInfo:
+	var max_scale := 0.0
+	var min_scale := INF
+	var refs: Dictionary # String, Array[Sprite2D]
+
+
+class TileInfo:
+	var nexts: Dictionary # Vector2, Array#[Vector2]
