7e3fc6f724d14cf3388f4e260b099d52cb7da940
Tom <tom@contextfree.info>
2024-03-30 06:16:22 -0700

    Adjust player speed

--- godot/player.gd
+++ godot/player.gd
@@ -2,6 +2,8 @@ class_name Player
 extends AnimatedSprite2D
 
 
+@export var speed := 550.0
+
 #func _ready():
 
 
@@ -9,14 +11,14 @@ func _process(delta: float):
 	var animation_speed = 1.0
 	var velocity := Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * 400 * delta
+		velocity = Vector2.UP.rotated(rotation)
 	elif Input.is_action_pressed("ui_down"):
-		velocity = Vector2.DOWN.rotated(rotation) * 400 * delta
+		velocity = Vector2.DOWN.rotated(rotation)
 	elif Input.is_action_pressed("ui_left"):
-		velocity = Vector2.LEFT.rotated(rotation) * 400 * delta
+		velocity = Vector2.LEFT.rotated(rotation)
 	elif Input.is_action_pressed("ui_right"):
 		animation_speed = 2.0
-		velocity = Vector2.RIGHT.rotated(rotation) * 400 * delta
+		velocity = Vector2.RIGHT.rotated(rotation)
 	play("swim", animation_speed)
 	if velocity.length() > 0:
-		position += velocity
+		position += velocity * speed * delta
